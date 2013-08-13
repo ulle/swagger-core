1af74202539c1fea73c624aa636156bafc1acce9
Tony Tam <fehguy@gmail.com>
2013-08-13 16:24:28 -0700

    added jackson subtype support per #280

--- modules/swagger-core/src/main/scala/com/wordnik/swagger/converter/SwaggerSchemaConverter.scala
+++ modules/swagger-core/src/main/scala/com/wordnik/swagger/converter/SwaggerSchemaConverter.scala
@@ -3,6 +3,8 @@ package com.wordnik.swagger.converter
 import com.wordnik.swagger.model._
 import com.wordnik.swagger.annotations.ApiModel
 
+import com.fasterxml.jackson.annotation.{ JsonTypeInfo, JsonSubTypes }
+
 import org.slf4j.LoggerFactory
 
 import scala.collection.mutable.LinkedHashMap
@@ -24,35 +26,39 @@ class SwaggerSchemaConverter
         val sortedProperties = new LinkedHashMap[String, ModelProperty]()
         p.sortWith(_._1 < _._1).foreach(e => sortedProperties += e._2 -> e._3)
 
-        // handle ApiModel annotations
-        Option(cls.getAnnotation(classOf[ApiModel])) match {
-          case Some(e) => {
-            sortedProperties.size match {
-              case 0 => None
-              case _ => Some(Model(
-                toName(cls),
-                toName(cls),
-                cls.getName,
-                sortedProperties,
-                toDescriptionOpt(cls),
-                Option(e.parent.getName),
-                Option(e.discriminator),
-                e.subTypes.map(_.getName).toList
-              ))
-            }
-          }
-          case _ => {
-            sortedProperties.size match {
-              case 0 => None
-              case _ => Some(Model(
-                toName(cls),
-                toName(cls),
-                cls.getName,
-                sortedProperties,
-                toDescriptionOpt(cls)
-              ))
-            }
-          }
+        val parent = Option(cls.getAnnotation(classOf[ApiModel])) match {
+          case Some(e) => Some(e.parent.getName)
+          case _ => None
+        }
+        val discriminator = {
+          val v = 
+            if(cls.getAnnotation(classOf[ApiModel]) != null)
+              cls.getAnnotation(classOf[ApiModel]).discriminator
+            else if(cls.getAnnotation(classOf[JsonTypeInfo]) != null)
+              cls.getAnnotation(classOf[JsonTypeInfo]).property
+            else ""
+          if(v != null && v != "") Some(v)
+          else None
+        }
+        val subTypes = {
+          if(cls.getAnnotation(classOf[ApiModel]) != null)
+            cls.getAnnotation(classOf[ApiModel]).subTypes.map(_.getName).toList
+          else if(cls.getAnnotation(classOf[JsonSubTypes]) != null)
+            (for(subType <- cls.getAnnotation(classOf[JsonSubTypes]).value) yield (subType.value.getName)).toList
+          else List()
+        }
+        sortedProperties.size match {
+          case 0 => None
+          case _ => Some(Model(
+            toName(cls),
+            toName(cls),
+            cls.getName,
+            sortedProperties,
+            toDescriptionOpt(cls),
+            parent,
+            discriminator,
+            subTypes
+          ))
         }
       }
     })
